dfbc2c3e7232ca162f75e984f0028c5fd7d0bfcc
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2022-11-16 11:03:13 +0200

    Add form tags and in form tags add heading of LOGIN
    - Action = "login.php": connect the file to form tags in index.php file
    
    - Method = "post" : hide user's information

--- Login/index.php
+++ Login/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
 </head>
 <body>
-    
+    <form action="login.php" method="post">
+        <h1>LOGIN</h1>
+
+    </form>
 </body>
 </html>
